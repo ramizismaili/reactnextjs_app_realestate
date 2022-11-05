349836923361a1c3347ce3e6c64067a0afcdb4ae
Ramiz Ismaili <rismaili49@gmail.com>
2022-11-06 00:54:09 +0100

    Search Filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -9,12 +9,47 @@ import {
   Icon,
   Button,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>searchFilters</div>;
+  const [filters, setFilters] = useState(filterData);
+  const searchProperties = (filterValues) => {
+    const path = useRouter.pathname;
+    const { query } = Router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+
+    Router.push({ pathname: path, query });
+  };
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
