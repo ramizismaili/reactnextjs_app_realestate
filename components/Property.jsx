b859f6b21b73bd52f75b7c3468c5b5822888b62f
Ramiz Ismaili <rismaili49@gmail.com>
2022-11-05 23:49:50 +0100

    Search Page

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/avatar";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
@@ -14,8 +15,8 @@ const Property = ({
     price,
     rentFrequency,
     rooms,
-    baths,
     title,
+    baths,
     area,
     agency,
     isVerified,
@@ -27,7 +28,7 @@ const Property = ({
       flexWrap="wrap"
       w="420px"
       p="5"
-      paddingTop="0"
+      paddingTop="0px"
       justifyContent="flex-start"
       cursor="pointer"
     >
@@ -36,41 +37,36 @@ const Property = ({
           src={coverPhoto ? coverPhoto.url : DefaultImage}
           width={400}
           height={260}
-          alt="house"
         />
-        <Box w="full">
-          <Flex
-            paddingTop="2"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Flex alignItems="center">
-              <Box paddingRight="3" color="green.400">
-                {isVerified && <GoVerified />}
-              </Box>
-              <Text fontWeight="bold" fontSize="lg">
-                AED {millify(price)}
-                {rentFrequency && `${rentFrequency}`}
-              </Text>
-            </Flex>
-            <Box>
-              <Avatar size="sm" src={agency?.logo?.url} />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
             </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {price}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
           </Flex>
-          <Flex
-            alignItems="center"
-            p="1"
-            justifyContent="space-between"
-            w="250px"
-            color="blue.400"
-          >
-            {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
-            <BsGridFill />
-          </Flex>
-          <Text fontSize="lg">
-            {title.length > 30 ? `${title.substring(0, 30)}...` : title}
-          </Text>
-        </Box>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          p="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
+          {rooms}
+          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+        <Text fontSize="lg">
+          {title.length > 30 ? title.substring(0, 30) + "..." : title}
+        </Text>
       </Box>
     </Flex>
   </Link>
